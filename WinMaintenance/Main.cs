d9e891700761104b7a487665c388d7986a0723c1
hibikaze6 <hibikaze6@gmail.com>
2021-08-17 18:21:43 +0900

    メモリの最大容量表示を一応追加

--- WinMaintenance/Main.cs
+++ WinMaintenance/Main.cs
@@ -21,13 +21,17 @@ namespace WinMaintenance
          * なんか処理を別にしないとフォーム固まる(Task.Runの中でできない件)
          * ここにはInvoke処理で値が変更される処理を書くところ
          */
-        private void setCpuProgressValue(int CpuUsePer)
+        private void setCpuProgressValue(int cpuUsePer)
         {
-            cpuProgress.Value = CpuUsePer;
+            cpuProgress.Value = cpuUsePer;
         }
-        private void setMemoryProgressValue(int MemoryUsePer)
+        private void setMemoryProgressValue(int memoryUsePer)
         {
-            memoryProgress.Value = MemoryUsePer;
+            memoryProgress.Value = memoryUsePer;
+        }
+        private void setMaxMemoryProgressValue(int memoryMaxValue) 
+        {
+            memoryProgress.Maximum = memoryMaxValue;
         }
         public Main()
         {
@@ -35,15 +39,31 @@ namespace WinMaintenance
         }
 
         //使えることがわかったらメソッド名ちゃんとかえる
-        private int test1()
+        private int cpuUsePercentChange()
         {
             AutoProps.managementClass = "Win32_Processor";
             AutoProps.classProperty = "LoadPercentage";
-            while (!formEnd_Flg)
-            {
-                Thread.Sleep(500);
-                Invoke(new del(setCpuProgressValue), int.Parse(WmiOperation.getWmiInfo()));
-            }
+            Thread.Sleep(500);
+            Invoke(new del(setCpuProgressValue), int.Parse(WmiOperation.getWmiInfo()));
+            return 1;
+        }
+        private int memoryUsePercentChange()
+        {
+            AutoProps.managementClass = "Win32_OperatingSystem";
+            AutoProps.classProperty = "TotalVisibleMemorySize";
+
+            var maxMemory = Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) / 10;
+
+            AutoProps.classProperty = "FreePhysicalMemory";
+
+            var freeMemory = Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) / 10;
+
+
+            MessageBox.Show(maxMemory.ToString());
+            MessageBox.Show(freeMemory.ToString());
+
+            Invoke(new del(setMaxMemoryProgressValue), maxMemory);
+            Invoke(new del(setMemoryProgressValue), freeMemory);
             return 1;
         }
         private async void Main_Load(object sender, EventArgs e)
@@ -52,12 +72,22 @@ namespace WinMaintenance
             AutoProps.classProperty = "Name";
             cpuNameLabel.Text = WmiOperation.getWmiInfo();
 
+            AutoProps.managementClass = "Win32_OperatingSystem";
+            AutoProps.classProperty = "TotalVisibleMemorySize";
 
-            //CPU使用率を別スレッドで変異させるTask.Run
-            Task<int> task1 = Task.Run(() => test1());
+            memoryAvailableLabel.Text = (Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) / 10).ToString();
 
-            //ここでTask全般を待機する(次はWhenAllとWaitAllの違いを覚えること)
-            await Task.WhenAll(task1);
+            while (!formEnd_Flg)
+            {
+                //CPU使用率を別スレッドで変異させるTask.Run
+                Task<int> task1 = Task.Run(() => cpuUsePercentChange());
+                //Task1を待機
+                await Task.WhenAll(task1);
+                //メモリ使用率を別スレッドで変異させるTask.Run
+                Task<int> task2 = Task.Run(() => memoryUsePercentChange());
+                //Task2を待機
+                await Task.WhenAll(task2);
+            }
         }
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
         {
