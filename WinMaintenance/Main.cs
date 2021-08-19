44199c5a807be3bea5dee7c0db88df6e2076903c
hibikaze6 <hibikaze6@gmail.com>
2021-08-19 20:37:47 +0900

    メモリの空き容量ProgressBarとLabelTextの機能実装

--- WinMaintenance/Main.cs
+++ WinMaintenance/Main.cs
@@ -14,12 +14,14 @@ namespace WinMaintenance
 {
     public partial class Main : Form
     {
-        delegate void del(int value);
+        delegate void delInt(int value);
+        delegate void delStr(string value);
+
         bool formEnd_Flg = false;
 
         /*
          * なんか処理を別にしないとフォーム固まる(Task.Runの中でできない件)
-         * ここにはInvoke処理で値が変更される処理を書くところ
+         * ここにはInvoke処理でFormの値が変更される処理を書くところ
          */
         private void setCpuProgressValue(int cpuUsePer)
         {
@@ -33,18 +35,24 @@ namespace WinMaintenance
         {
             memoryProgress.Maximum = memoryMaxValue;
         }
+        private void setMemoryAvailableLabelText(string memoryAvailableLabelText)
+        {
+            memoryAvailableLabel.Text = memoryAvailableLabelText;
+        }
+        /*
+         * ここにはInvoke処理でFormの値が変更される処理を書くところ ～End～
+         */
+
         public Main()
         {
             InitializeComponent();
         }
-
-        //使えることがわかったらメソッド名ちゃんとかえる
         private int cpuUsePercentChange()
         {
             AutoProps.managementClass = "Win32_Processor";
             AutoProps.classProperty = "LoadPercentage";
             Thread.Sleep(500);
-            Invoke(new del(setCpuProgressValue), int.Parse(WmiOperation.getWmiInfo()));
+            Invoke(new delInt(setCpuProgressValue), int.Parse(WmiOperation.getWmiInfo()));
             return 1;
         }
         private int memoryUsePercentChange()
@@ -52,18 +60,23 @@ namespace WinMaintenance
             AutoProps.managementClass = "Win32_OperatingSystem";
             AutoProps.classProperty = "TotalVisibleMemorySize";
 
-            var maxMemory = Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) / 10;
+            //メモリ最大を○○.○GB表記にするためにdoubleParceしてる
+            var maxMemory = (Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) / 10);
 
             AutoProps.classProperty = "FreePhysicalMemory";
 
-            var freeMemory = Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) / 10;
+            //空きメモリ要領を 〃
+            var freeMemory = (Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) /10);
 
-
+            /* 値の確認テスト用
             MessageBox.Show(maxMemory.ToString());
             MessageBox.Show(freeMemory.ToString());
+            */
 
-            Invoke(new del(setMaxMemoryProgressValue), maxMemory);
-            Invoke(new del(setMemoryProgressValue), freeMemory);
+            Invoke(new delInt(setMaxMemoryProgressValue), Convert.ToInt32(maxMemory * 10));
+            Invoke(new delInt(setMemoryProgressValue), (memoryProgress.Maximum - Convert.ToInt32(freeMemory * 10)));
+            Invoke(new delStr(setMemoryAvailableLabelText), ("UseMem" + maxMemory + " / " + (maxMemory - freeMemory).ToString()));
+
             return 1;
         }
         private async void Main_Load(object sender, EventArgs e)
@@ -72,27 +85,22 @@ namespace WinMaintenance
             AutoProps.classProperty = "Name";
             cpuNameLabel.Text = WmiOperation.getWmiInfo();
 
-            AutoProps.managementClass = "Win32_OperatingSystem";
-            AutoProps.classProperty = "TotalVisibleMemorySize";
-
-            memoryAvailableLabel.Text = (Math.Floor(double.Parse(WmiOperation.getWmiInfo()) / 1024.0 / 1024.0 * 10) / 10).ToString();
-
             while (!formEnd_Flg)
             {
-                //CPU使用率を別スレッドで変異させるTask.Run
-                Task<int> task1 = Task.Run(() => cpuUsePercentChange());
+                //メモリ使用率を別スレッドで変異させるTask.Run
+                Task<int> task1 = Task.Run(() => memoryUsePercentChange());
                 //Task1を待機
                 await Task.WhenAll(task1);
-                //メモリ使用率を別スレッドで変異させるTask.Run
-                Task<int> task2 = Task.Run(() => memoryUsePercentChange());
+                //CPU使用率を別スレッドで変異させるTask.Run
+                Task<int> task2 = Task.Run(() => cpuUsePercentChange());
                 //Task2を待機
                 await Task.WhenAll(task2);
             }
         }
-        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        private async void Main_FormClosing(object sender, FormClosingEventArgs e)
         {
             formEnd_Flg = true;
-            Thread.Sleep(1000);
+            await Task.WhenAll();
         }
 
         private void applyButton_Click(object sender, EventArgs e)
